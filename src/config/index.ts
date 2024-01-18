bcc805d0c9b882e1dd1975e19331171f1e7b17f0
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2024-01-18 20:35:33 +1100

    Started to implement navbar functionality
    - Introduced a new `Navbar` component with a `Gem` icon and `NavItems`
    - Developed `NavItems` component to create a `NavItem` for each category in `PRODUCT_CATEGORIES`
    - Implemented toggle functionality in `NavItem` to open and close categories
    - Added a `PRODUCT_CATEGORIES` constant in `index.ts` for category information

---
+++ src/config/index.ts
@@ -0,0 +1,86 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "Art & Collectibles",
+    value: "art-collectibles",
+    featured: [
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: "/nav/art-collectibles/",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/art-collectibles/",
+      },
+      {
+        name: "Editor pics",
+        href: "#",
+        imageSrc: "/nav/art-collectibles/",
+      },
+    ],
+  },
+  {
+    label: "Health & Beauty",
+    value: "health-beauty",
+    featured: [
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: "/nav/health-beauty/",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/health-beauty/",
+      },
+      {
+        name: "Editor pics",
+        href: "#",
+        imageSrc: "/nav/health-beauty/",
+      },
+    ],
+  },
+  {
+    label: "Home & Living",
+    value: "home-living",
+    featured: [
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: "/nav/home-living/",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/home-living/",
+      },
+      {
+        name: "Editor pics",
+        href: "#",
+        imageSrc: "/nav/home-living/",
+      },
+    ],
+  },
+  {
+    label: "Jewelry & Accessories",
+    value: "jewelry-accessories",
+    featured: [
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: "/nav/jewelry-accessories/",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/jewelry-accessories/",
+      },
+      {
+        name: "Editor pics",
+        href: "#",
+        imageSrc: "/nav/jewelry-accessories/",
+      },
+    ],
+  },
+];
